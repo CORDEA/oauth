cf44e71166769781b32c440a10f873595f5bf088
Yoshihiro Tanaka <contact@cordea.jp>
2019-12-22 15:02:52 +0900

    authorizationCodeGrant is deprecated

--- examples/slack.nim
+++ examples/slack.nim
@@ -14,28 +14,47 @@
 # Author: Yoshihiro Tanaka <contact@cordea.jp>
 # date  :2016-03-08
 
+import uri
+import json
 import oauth2
 import httpclient
-import json
 
 const
     authorizeUrl = "https://slack.com/oauth/authorize"
     accessTokenUrl = "https://slack.com/api/oauth.access"
+    redirectUri = "http://localhost:8080"
     url = "https://slack.com/api/channels.list"
-
-let html = "resources/index.html".readFile()
 
 echo "Please enter the client id."
 let clientId = readLine(stdin)
 echo "Please enter the client secret."
 let clientSecret = readLine(stdin)
 
-echo "Please go to this url."
 let
-    client = newHttpClient()
-    response = client.authorizationCodeGrant(authorizeUrl, accessTokenUrl,
-        clientId, clientSecret, html, scope = @["channels:read"])
-    obj = parseJson(response.body)
-    accessToken = obj["access_token"].str
-    r = client.getContent(url & "?token=" & accessToken)
+  state = generateState()
+  grantUrl = getAuthorizationCodeGrantUrl(authorizeUrl, clientId, redirectUri, state, @["channels:read"])
+echo "Please go to this url."
+echo grantUrl
+
+# Receives redirect url. You can also handle directly from server that was launched.
+echo "Please enter the received redirect url."
+# ex. https://example.com?code=xxxxxxxxxx&state=xxxxx
+let
+  receivedUri = readLine(stdin)
+  grantResponse = receivedUri.parseAuthorizationCodeGrantRedirectUri()
+
+assert state == grantResponse.state
+
+let
+  client = newHttpClient()
+  response = client.getAuthorizationCodeAccessToken(
+    accessTokenUrl,
+    grantResponse.code,
+    clientid,
+    clientSecret,
+    redirectUri
+  )
+  obj = parseJson(response.body)
+  accessToken = obj["access_token"].str
+  r = client.getContent(url & "?token=" & accessToken)
 echo r
